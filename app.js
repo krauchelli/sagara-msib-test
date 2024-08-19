13b296ee8eb0ba9d041ac05aac404de6038c634b
Krauchelli <mekaku26shi@gmail.com>
2024-08-19 19:06:15 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const methodOverride = require('method-override');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+const host = 'localhost';
+const port = process.env.PORT || 3000;
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // jika mengirimkan data dalam bentuk form
+
+// route
+app.use('/api', require('./src/routes/index.routes'));
+
+
+// listener
+app.listen(port, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
